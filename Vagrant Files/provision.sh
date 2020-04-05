2d29ba602c0eead521611214d200e47227eae119
CullynMillar <mcullyn@gmail.com>
2020-04-05 23:41:39 +1000

    updated line 4 and 6 to include force yes commands

--- Vagrant Files/provision.sh
+++ Vagrant Files/provision.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo apt-get update
 # install Docker onto the dev environment to compile docker images
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 #Install NodeJS for the NodeJS application compilation automation
-sudo apt install nodejs
+sudo apt install nodejs -y
